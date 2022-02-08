c94daf4bc99c9628df88df3661fff47256976e2c
Akhmad Wildan <wildanakhmad3584@gmail.com>
2022-02-09 06:47:41 +0700

    create stream from collection, create stream infinite

--- src/test/java/belajar/java/stream/CreateStreamTest.java
+++ src/test/java/belajar/java/stream/CreateStreamTest.java
@@ -3,6 +3,8 @@ package belajar.java.stream;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class CreateStreamTest {
@@ -25,5 +27,25 @@ public class CreateStreamTest {
         String[] myNames = {"Wildan", "Akhmad", "Danu", "Nanu"};
         Stream<String> stringStream = Arrays.stream(myNames);
         stringStream.forEach(System.out::println);
+    }
+
+    @Test
+    void testCreateStreamFromCollectionInterface() {
+        Collection<String> stringCollection = List.of("Akhmad", "Wildan", "Danu");
+        Stream<String> stringStream = stringCollection.stream();
+        stringStream.forEach(System.out::println);
+        Stream<String> stringStream2 = stringCollection.stream();
+        stringStream2.forEach(System.out::println);
+        // Karena stream adalah aliran maka tidak bisa dijalankan lebih dari satu kali,
+        // Jika dijalankan lebih dari satu akan terjadi error,
+        // Solusinya adalah menginisiasi stream baru.
+    }
+
+    @Test
+    void testCreateInfiniteStream() {
+        Stream<String> stringStream = Stream.generate(() -> "Akhmad Wildan");
+        // stringStream.forEach(System.out::println);
+        Stream<Integer> integerStream = Stream.iterate(1, value -> value + 1);
+        // integerStream.forEach(System.out::println);
     }
 }
